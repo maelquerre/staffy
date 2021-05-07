452e880eacae3896325c7516f7fe06499374a459
Maël Querré <mael.querre@gmail.com>
2021-05-07 16:54:01 +0200

    Add Moncaco editor with its options

--- resources/js/views/Score.vue
+++ resources/js/views/Score.vue
@@ -1,24 +1,45 @@
 <template>
   <div class="flex-grow flex flex-col">
     <div class="flex-grow flex">
+      <!--
       <textarea
+        id="content"
         v-model="content"
         class="w-1/2 font-mono"
       />
+      -->
+      <div
+        class="w-1/2">
+        <MonacoEditor
+          id="editor"
+          v-model="content"
+          :options="monacoOptions"
+          style="width: 600px; height: 800px;"
+        />
+      </div>
+
 
       <div
-        class="w-1/2 bg-gray-100"
+        class="w-1/2 p-4 bg-gray-100"
       >
-        preview
+        <div
+          id="preview"
+          class="bg-white"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
+import abcjs from 'abcjs'
+import MonacoEditor from 'vue-monaco'
 
 export default {
+  components: {
+    MonacoEditor,
+  },
+
   data() {
     return {
       content: `X:1
@@ -41,7 +62,73 @@ K:HP
 \\
 |f/2a3/2 {fg}fa | A2 {Gdc}d3/2e/2 | {g}fd {gf}gf | {g}fe {A} e{gf}g |
 \\
-|a3/2g/2 {fg}fa | A2 {Gdc}d3/2e/2 | {g}f2 {gf}ge | {Gdc}d2 {gdG}d |]`
+|a3/2g/2 {fg}fa | A2 {Gdc}d3/2e/2 | {g}f2 {gf}ge | {Gdc}d2 {gdG}d |]`,
+
+      monacoOptions: {
+        lineDecorationsWidth: 16,
+        lineNumbers: 'off',
+        lineNumbersMinChars: 3,
+        wordWrap: 'off',
+        wrappingIndent: 'same',
+        scrollBeyondLastLine: false,
+        scrollBeyondLastColumn: 2,
+        smoothScrolling: true,
+        useTabStops: true,
+        showUnused: false,
+        folding: false,
+        cursorBlinking: 'smooth',
+        renderLineHighlight: 'none',
+        renderWhitespace: 'selection',
+        renderControlCharacters: false,
+        dragAndDrop: true,
+        fontSize: 12,
+        disableMonospaceOptimizations: false,
+        extraEditorClassName: 'scripter-light',
+        links: false,
+        codeLens: false,
+        lightbulb: {
+          enabled: false
+        },
+        quickSuggestions: true,
+        quickSuggestionsDelay: 800,
+        acceptSuggestionOnEnter: 'smart',
+        suggestSelection: 'recentlyUsedByPrefix',
+        tabCompletion: 'on',
+        hover: {
+          enabled: true,
+          delay: 1e3
+        },
+        suggest: {
+          localityBonus: true,
+          shareSuggestSelections: true,
+          hideStatusBar: false
+        },
+        scrollbar: {
+          useShadows: false,
+          verticalScrollbarSize: 9,
+          verticalSliderSize: 5,
+          horizontalScrollbarSize: 9,
+          horizontalSliderSize: 5
+        },
+        minimap: {
+          enabled: false
+        }
+      }
+    }
+  },
+
+  mounted() {
+    this.initEditor()
+  },
+
+  methods: {
+    initEditor() {
+      new abcjs.Editor('editor', {
+        canvas_id: 'preview',
+        abcjsParams: {
+          responsive: 'resize',
+        },
+      })
     }
   }
 }
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,7 +6,10 @@
 
     <title>{{ env('APP_NAME') }}</title>
 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
